7ef6d5c93864c063ae26bf03d6df5b07af64c533
Anirud Shrestha <anirudstha5@gmail.com>
2020-09-16 18:56:15 +0545

    Updates

---
+++ precache-manifest.d9ba0d9fa9a58039ae44408008913008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e894831149be82d22b344c2922c1c4e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "bd571993b381e5c7a504",
+    "url": "/covid-19-tracker/static/css/main.2d069749.chunk.css"
+  },
+  {
+    "revision": "ac9667f6ef370ef99ed0",
+    "url": "/covid-19-tracker/static/js/2.c162e04f.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19-tracker/static/js/2.c162e04f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd571993b381e5c7a504",
+    "url": "/covid-19-tracker/static/js/main.cfd44711.chunk.js"
+  },
+  {
+    "revision": "ba15ed3d26f88df4a146",
+    "url": "/covid-19-tracker/static/js/runtime-main.425a1808.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
